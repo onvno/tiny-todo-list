2346810e5eb7ad635b0b91c36c0b1c8711b856cd
onvno <leewei2020@126.com>
2019-09-22 18:27:59 +0800

    init drag

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Comp from './Comp.svelte';
 	import ListCard from './ListCard.svelte';
+	import BindComp from './BindComp.svelte';
 
 	export let name;
 
@@ -23,6 +24,7 @@
 <div class="container">
 	<Comp />
 	<ListCard />
+	<BindComp />
 	<!-- <h1>字體庫不是很完整，試試繁體: {name}!</h1> -->
 	<!-- <button on:click={handleClick}>
 		Clicked {count} {count === 1 ? 'time' : 'times'}
---
+++ src/BindComp.svelte
@@ -0,0 +1,39 @@
+<div>
+  <p bind:this={element}  on:mousedown={downFunc}>111111111111</p>
+  <p>222222222222</p>
+  <p>222222222222</p>
+</div>
+
+<script>
+  import {onMount, onDestroy, getContext} from 'svelte';
+  
+  let element
+
+  function downFunc(event){
+    console.log("mouseDown")
+
+    document.onmousemove = function(e) {
+      console.log(e.clientX)
+    }
+  }
+
+  function moveFunc(event) {
+    console.log("move:", event.clientX)
+  }
+
+  onMount(() => {
+    console.log('the component has mounted');
+    console.log("element:", element);
+    
+    // element.addEventListener('click', function(){
+    //   console.log('click!!!');
+    // })
+
+    document.onmouseup = function(event){
+      document.onmousemove = null;
+    }
+
+  });
+  
+
+</script>
